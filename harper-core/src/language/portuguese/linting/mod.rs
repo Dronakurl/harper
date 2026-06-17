e275d5c0d61e2ab4f2d9001b8ba5c7f544a22353
konrad <>
2026-06-17 20:58:52 +0200

    add portuguese weir rules

--- harper-core/src/language/portuguese/linting/mod.rs
+++ harper-core/src/language/portuguese/linting/mod.rs
@@ -1,3 +1,4 @@
 //! Portuguese linting rules and checkers.
 
 pub mod portuguese_spell_check;
+pub mod weir_rules;
---
+++ harper-core/src/language/portuguese/linting/weir_rules/mod.rs
@@ -0,0 +1,50 @@
+use crate::linting::LintGroup;
+
+// This module provides the infrastructure for Portuguese Weir rules
+// Even though there are no rules yet, the structure is in place for future additions
+
+macro_rules! generate_boilerplate {
+    ([$($name:ident),+ $(,)?]) => {
+        pub fn lint_group() -> LintGroup {
+            let mut group = LintGroup::default();
+
+            {
+                $(
+                    let linter = WeirLinter::new(include_str!(concat!(env!("PORTUGUESE_WEIR_RULE_DIR"), "/", stringify!($name), ".weir"))).unwrap();
+                    match linter.into_sentence_linter() {
+                        Ok(linter) => group.add_sentence_expr_linter(stringify!($name), linter),
+                        Err(linter) => group.add_chunk_expr_linter(stringify!($name), linter.into_chunk_linter().unwrap_or_else(|_| unreachable!())),
+                    };
+                )+
+            }
+
+            group.set_all_rules_to(Some(true));
+
+            group
+        }
+
+        #[cfg(test)]
+        mod tests {
+            use paste::paste;
+            use crate::weir::tests::assert_passes_all;
+            use crate::weir::WeirLinter;
+
+            $(
+                paste! {
+                    #[test]
+                    fn [<run_tests_for_ $name:snake>](){
+                        let mut linter = WeirLinter::new(include_str!(concat!(env!("PORTUGUESE_WEIR_RULE_DIR"), "/", stringify!($name), ".weir"))).unwrap();
+                        assert_passes_all(&mut linter);
+                    }
+                }
+            )+
+        }
+    };
+    ([$(,)?]) => {
+        pub fn lint_group() -> LintGroup {
+            LintGroup::default()
+        }
+    };
+}
+
+include!(env!("PORTUGUESE_WEIR_RULE_LIST"));
